1fd3028ae32b9463400b70eb3e6f19df17430695
Shae ONeil <shae-oneil@uiowa.eduinital commitgit add shae.txtgit commit -m 'initial commit #2'git add shae.txgit config --global user.email shae-oneil@uiowa.edu>
2022-04-03 19:49:12 -0500

    changes made

--- app.py
+++ app.py
@@ -3,31 +3,23 @@ from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-friend_list = [{"name": "Mike Colbert", "email":"mike@mike.com" } ]
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Mike\'s Friends', friends = friend_list)
+    return render_template('index.html', pageTitle='Shae\'s Friends')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', pageTitle='About ')
+    return render_template('about.html', pageTitle='About Page')
 
 @app.route('/estimate', methods=['GET', 'POST'])
 def estimate():
     if request.method == 'POST':
         form = request.form
-        fname = form['fname']
-        lname = form['lname']
-        email = form['email']
-        friend_dict = {"name": fname + " " + lname, "email": email}
-        friend_list.append(friend_dict)
-        return redirect(url_for('index'))
-    return redirect(url_for('index'))
-    
-@app.route('/base')
-def base():
-    return render_template('base.html', pageTitle=' Vertical Tank Maintenance')
-
+        radius = float(form['radius'])
+        height = float(form['height'])
+        quote = radius+height
+        return render_template('estimate.html', pageTitle='Estimate', estimate=quote)
+    return render_template('estimate.html', pageTitle='Estimate',)
 if __name__ == '__main__':
     app.run(debug=True)
